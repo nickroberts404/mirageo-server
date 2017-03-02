96b7ee2dc4799770c8ac95414bbc8b9b0eb8f29d
nickroberts404 <nickroberts404@gmail.com>
2017-03-02 09:14:20 -0600

    Added mapKey adding/editing to a local db

--- settings.js
+++ settings.js
@@ -2,18 +2,28 @@
 // Uses commander.js to parse CLI arguments.
 
 var program = require('commander');
+const low = require('lowdb');
+const db = low('db.json');
+
+db.defaults({ mapkey: '' })
+	.write()
 
 const defaults = {
 	count: 100,
 	bound: [90, -180, -90, 180], // equivalent to "the whole earth!"
 	geojson: false,
-	port: 3030,
 }
 
-var parseBBox = (i) => {
+const parseBBox = (i) => {
 	i = i.split(',').map(parseFloat); // Turns "12,-83.1,-27,33.3" into [12, -83.1, -27, 33.3]
 	if(i.length !== 4) return defaults.bound; // Invalid bbox
 	return [i[0], i[1], i[2], i[3]];
+}
+
+const addMapKey = (key) => {
+	db.set('mapkey', key).write();
+	console.log(`Map key has been changed to ${key}`)
+	return key;
 }
 
 program
@@ -21,10 +31,16 @@ program
 	.option('-c, --count <c>', 'The amount of points to create <int>', i => parseInt(i), defaults.count)
 	.option('-b, --bound <b>', 'The bounding box, NW to SE <lat1,lng1,lat2,lng2>', parseBBox, defaults.bound)
 	.option('-g, --geojson', 'Send data as geoJSON objects', defaults.geojson)
-	.option('-p, --port <p>', 'Which port the server will run on, defaults to 3030', defaults.port)
+	.option('-p, --port <p>', 'Which port the server will run on, defaults to 3030')
+	.option('-m, --map-key [m]', 'Add Mapbox API key for the interface', addMapKey)
 	.parse(process.argv);
 
-// Perform a union of defaults and programs attributes
-const p = Object.keys(defaults).reduce((o, k) => Object.assign(o, {[k]: program[k]}), {})
+// MapKey flag was used, but no arg. Just display the current MapKey.
+if(program.mapKey === true) {
+	const key = db.get('mapkey').value();
+	console.log(`Current map key set to ${key}`);
+} 
+// // Perform a union of defaults and programs attributes
+// const p = Object.keys(defaults).reduce((o, k) => Object.assign(o, {[k]: program[k]}), {})
 // Export a union of defaults and program arguments, program arguments taking priority
-module.exports = Object.assign({}, defaults, p);
+module.exports = Object.assign({}, defaults, program);
