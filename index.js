f46e0134c6744d85ba215a8f1326689e82fed7a3
nickroberts404 <nickroberts404@gmail.com>
2017-03-04 15:45:14 -0600

    Changed dependency versions, and server can now be called from any directory

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 const port = process.env.PORT || settings.port || 3030;
 
 // // Basic Middleware
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
